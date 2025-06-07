aa8ac7c5f359ad591b5d8cc730cc08ec64bdf0be
ncuaspudt <nm.cuaspudt@uea.edu.ec>
2025-06-06 20:18:21 -0500

    Borrar linea 3,4,5 de impreiòn de texto y agregar un nombre

--- EstructuraApptareas2025/Program.cs
+++ EstructuraApptareas2025/Program.cs
@@ -1,7 +1,8 @@
 ﻿//Clases para dos figuras geométricas(circulo, cuadrado) en código C#.
 using System;
-Console.WriteLine("Universidad Estatal Amazónica");
-Console.WriteLine("Ingenieria en TICS");
-Console.WriteLine("Estructura de datos");
+
+
+namespace FigurasGeometricas
+
 
 
