c33ea5504ef7891d0ec32ffd14a2b24b627ed181
ncuaspudt <nm.cuaspudt@uea.edu.ec>
2025-06-07 08:09:34 -0500

    Agrego un contructor para iniciar el radio del Círculo

--- EstructuraApptareas2025/Program.cs
+++ EstructuraApptareas2025/Program.cs
@@ -5,6 +5,19 @@ using System;
 namespace FigurasGeometricas
 
 //Clase círculo para representar el círculo y calcular área y perímetro
+public class Circulo
+{
+    private doble radio; //Atributo privado que encapsula el valor del radio del círculo
+
+    //Constructor de la clase Círculo
+    public Circulo(double radio)
+
+
+
+
+
+
+}
 
 
 
