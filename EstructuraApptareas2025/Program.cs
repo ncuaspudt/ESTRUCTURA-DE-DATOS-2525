3ea02e58e1c648409f12b0c0e3496985284768f4
ncuaspudt <nm.cuaspudt@uea.edu.ec>
2025-06-06 19:52:27 -0500

    Comentario en primera linea

--- EstructuraApptareas2025/Program.cs
+++ EstructuraApptareas2025/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Universidad Estatal Amazónica");
+﻿//Clases para dos figuras geométricas(circulo, cuadrado) en código C#.
+Console.WriteLine("Universidad Estatal Amazónica");
 Console.WriteLine("Ingenieria en TICS");
 Console.WriteLine("Estructura de datos");
 
