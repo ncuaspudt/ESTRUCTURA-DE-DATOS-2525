2f8e4de511b7ec42ec85c71b34649a3c2a8206c3
ncuaspudt <nm.cuaspudt@uea.edu.ec>
2025-06-06 20:09:00 -0500

    Aumento de using System

--- EstructuraApptareas2025/Program.cs
+++ EstructuraApptareas2025/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Clases para dos figuras geométricas(circulo, cuadrado) en código C#.
+using System;
 Console.WriteLine("Universidad Estatal Amazónica");
 Console.WriteLine("Ingenieria en TICS");
 Console.WriteLine("Estructura de datos");
