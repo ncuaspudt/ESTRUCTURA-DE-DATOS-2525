be749d760babf5dd65636be66960d08663ef3e75
ncuaspudt <nm.cuaspudt@uea.edu.ec>
2025-06-12 22:51:47 -0500

    Método para visuailizar los datos del estudiante

--- ConsoleApp1/Arrays y matrices.cs
+++ ConsoleApp1/Arrays y matrices.cs
@@ -1,12 +1,19 @@
 ﻿// Implementare un clase Estudiante con varios atributos.
 // Mediante un array permitira almacenar tres números de teléfono.
 
-// Regisro de una clase llamada Estudiante.
+// Registro de una clase llamada Estudiante.
 class Estudiante
 {
     public int Id;                              //Atributo de identificador único
     public string Nombres;                      //Atributo nombres del estudiante
     public string Apellidos;                     //Atributo apellidos del estudiante
     public string Direccion;                     //Atributo dirección del estudiante
-    public string[] Telefonos;new string [3];    //Array para registar tres números de teléfono
+    public string[] Telefonos; new string[3];    //Array para registar tres números de teléfono
+    
+//Método que permite visualizar toda la información del estudiante
+    public void Mostrar datos()
+    {
+        Console.WriteLine("---Informacion del Estudiante---");
+        Console.WriteLine()
+}
 }
