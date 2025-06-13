542950a3f8208f07921769252681a4c12d2f938e
ncuaspudt <nm.cuaspudt@uea.edu.ec>
2025-06-12 22:21:53 -0500

    Agrege la clase Estudiante y un atributo

--- ConsoleApp1/Arrays y matrices.cs
+++ ConsoleApp1/Arrays y matrices.cs
@@ -1,2 +1,6 @@
 ﻿// Implementare un clase Estudiante con varios atributos.
 // Mediante un array permitira almacenar tres números de teléfono.
+class Estudiante
+{
+    public int Id; //Atributo de identificador único
+}
