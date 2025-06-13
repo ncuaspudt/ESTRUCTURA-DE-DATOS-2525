98e1d78f9fcd071dfa4b0c0c464f25917d5d494d
ncuaspudt <nm.cuaspudt@uea.edu.ec>
2025-06-12 22:38:38 -0500

    Ingreso un array para guardar los números de teléfono y comentario

--- ConsoleApp1/Arrays y matrices.cs
+++ ConsoleApp1/Arrays y matrices.cs
@@ -4,9 +4,9 @@
 // Regisro de una clase llamada Estudiante.
 class Estudiante
 {
-    public int Id;                 //Atributo de identificador único
-    public string Nombres;         //Atributo nombres del estudiante
-    public string Apellidos;       //Atributo apellidos del estudiante
-    public string Direccion;       //Atributo dirección del estudiante
-    
+    public int Id;                              //Atributo de identificador único
+    public string Nombres;                      //Atributo nombres del estudiante
+    public string Apellidos;                     //Atributo apellidos del estudiante
+    public string Direccion;                     //Atributo dirección del estudiante
+    public string[] Telefonos;new string [3];    //Array para registar tres números de teléfono
 }
