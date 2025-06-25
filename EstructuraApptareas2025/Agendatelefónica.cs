0fdb3d92503738626ae9028cf72523b4eedd27a8
ncuasput <nm.cuaspudt@uea.edu.ec>
2025-06-24 21:17:14 -0500

    Métodos y comentarios

--- EstructuraApptareas2025/Agendatelefónica.cs
+++ EstructuraApptareas2025/Agendatelefónica.cs
@@ -1,2 +1,7 @@
 // Se creara un sistema de Agenda telefónica.
 //Mediante la utilización de clases, métodos, atributos.
+//agregamos un métodos para escribir texto.
+
+Console.WriteLine("Universidad Estatal Amazónica");
+Console.WriteLine("Agenda telefónica");
+Console.WriteLine("===================");
