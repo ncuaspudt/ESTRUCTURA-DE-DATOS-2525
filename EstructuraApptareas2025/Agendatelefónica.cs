8c34100303a8a33468b97dfd269230e5d528d0b3
ncuasput <nm.cuaspudt@uea.edu.ec>
2025-06-25 08:41:05 -0500

    Elimina parte del codigo

--- EstructuraApptareas2025/Agendatelefónica.cs
+++ EstructuraApptareas2025/Agendatelefónica.cs
@@ -1,10 +1,6 @@
 // Se creara un sistema de Agenda telefónica.
 //Mediante la utilización de clases, métodos, atributos.
 //agregamos un métodos para escribir texto.
-using System;
-
-namespace Agendatelefonica
-{
 //Se define una estructura pública
 public struct Contacto
 {
