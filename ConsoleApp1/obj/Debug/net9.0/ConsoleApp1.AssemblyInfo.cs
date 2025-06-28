2d4424d2d583be90a8df5065bfe66998df78c6bc
ncuasput <nm.cuaspudt@uea.edu.ec>
2025-06-28 18:51:43 -0500

    Gitinore

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd8073397091d6769b0495c2dffc98753aa209e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8ce468bd5fec07ffc76bc06b2a97c2df1caf14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
