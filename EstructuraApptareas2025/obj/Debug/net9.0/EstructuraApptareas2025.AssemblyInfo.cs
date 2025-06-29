f32a5429bee0ddb3c77fdd8380657a5e5e7c9217
ncuasput <nm.cuaspudt@uea.edu.ec>
2025-06-28 23:52:19 -0500

    Inicio de comentarios y clases

--- EstructuraApptareas2025/obj/Debug/net9.0/EstructuraApptareas2025.AssemblyInfo.cs
+++ EstructuraApptareas2025/obj/Debug/net9.0/EstructuraApptareas2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstructuraApptareas2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8488cad4d2f28361ef49558d63d003ab4e4f152")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ffe188f67e6b8bcd2aab583b9d59aaca23f2460")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstructuraApptareas2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstructuraApptareas2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
