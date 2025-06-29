a9421351d2f11fa72987c0945135bcdc4d92a84b
ncuasput <nm.cuaspudt@uea.edu.ec>
2025-06-29 13:29:47 -0500

    Corrección en error de sintaxis

--- EstructuraApptareas2025/Asignaturas.cs
+++ EstructuraApptareas2025/Asignaturas.cs
@@ -1,9 +1,4 @@
-// Realizare un programa que asigne asignaturas de un curso con POO.
-
-using System;
-using System.Collections.Generic;
-
-// Clase  asignatura
+// Clase asignatura
 public class Asignaturas
 {
     // Propiedad para el nombre de la asignatura
@@ -26,27 +21,26 @@ public class Asignaturas
 public class Curso
 {
     // Lista privada de asignaturas
-    private List<Asignaturas> asignaturas;
+    private System.Collections.Generic.List<Asignaturas> asignaturas;
 
     // Constructor que inicializa la lista con algunas asignaturas
     public Curso()
     {
-        asignaturas = new List<Asignaturas>();
+        asignaturas = new System.Collections.Generic.List<Asignaturas>();
         asignaturas.Add(new Asignaturas("Matemáticas"));
         asignaturas.Add(new Asignaturas("Física"));
         asignaturas.Add(new Asignaturas("Lengua y Literatura"));
         asignaturas.Add(new Asignaturas("Historia"));
         asignaturas.Add(new Asignaturas("Química"));
-     
     }
 
     // Método público para mostrar las asignaturas
     public void MostrarAsignaturas()
     {
-        Console.WriteLine("Asignaturas del curso:");
-        foreach (var asignatura in asignaturas)
+        System.Console.WriteLine("Asignaturas del curso:");
+        foreach (Asignaturas asignatura in asignaturas)
         {
-            Console.WriteLine("_ " + asignatura.ObtenerNombre());
+            System.Console.WriteLine("_ " + asignatura.ObtenerNombre());
         }
     }
 }
