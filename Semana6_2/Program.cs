d79f25fbe031e37ef7585263215e50a54d41eebd
ncuasput <nm.cuaspudt@uea.edu.ec>
2025-07-05 13:19:40 -0500

    Se agregan comentarios y clase Nodo

--- Semana6_2/Program.cs
+++ Semana6_2/Program.cs
@@ -1,2 +1,132 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crear una lista enlazada con 50 números enteros, del 1 al 999 generados aleatoriamente. 
+// Se deben eliminar los nodos que estén fuera rango de valores leídos desde el teclado.
+// Clase que representa un nodo
+public class Nodo
+{
+    public int Data;
+    public Nodo Next;
+
+    public Nodo(int data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+// Lista enlazada simple
+public class ListaSimple
+{
+    Nodo head;
+
+    public ListaSimple()
+    {
+        head = null;
+    }
+
+    public void InsertarFinal(int dato)
+    {
+        Nodo nuevoNodo = new Nodo(dato);
+        if (head == null)
+        {
+            head = nuevoNodo;
+        }
+        else
+        {
+            Nodo actual = head;
+            while (actual.Next != null)
+            {
+                actual = actual.Next;
+            }
+            actual.Next = nuevoNodo;
+        }
+    }
+
+    public void DibujarLista()
+    {
+        Nodo actual = head;
+        System.Console.Write("head -->");
+        while (actual != null)
+        {
+            System.Console.Write(" [ " + actual.Data + " | * ]");
+            if (actual.Next != null)
+            {
+                System.Console.Write(" -->");
+            }
+            actual = actual.Next;
+        }
+        System.Console.Write(" --> null\n");
+    }
+
+    public int ContarElementos()
+    {
+        int contador = 0;
+        Nodo actual = head;
+        while (actual != null)
+        {
+            contador++;
+            actual = actual.Next;
+        }
+        return contador;
+    }
+
+    // Método para eliminar nodos fuera del rango [min, max]
+    public void EliminarFueraDeRango(int min, int max)
+    {
+        // Eliminar nodos del principio que estén fuera del rango
+        while (head != null && (head.Data < min || head.Data > max))
+        {
+            head = head.Next;
+        }
+
+        Nodo actual = head;
+        while (actual != null && actual.Next != null)
+        {
+            if (actual.Next.Data < min || actual.Next.Data > max)
+            {
+                actual.Next = actual.Next.Next;
+            }
+            else
+            {
+                actual = actual.Next;
+            }
+        }
+    }
+}
+
+// Clase principal
+class Program
+{
+    static void Main(string[] args)
+    {
+        ListaSimple lista = new ListaSimple();
+        System.Random rnd = new System.Random();
+
+        // Agregar 50 números aleatorios del 1 al 999
+        for (int i = 0; i < 50; i++)
+        {
+            int numero = rnd.Next(1, 1000);
+            lista.InsertarFinal(numero);
+        }
+
+        // Mostrar lista inicial
+        System.Console.WriteLine("Lista original:");
+        lista.DibujarLista();
+
+        // Leer rango desde teclado
+        System.Console.Write("Ingrese el valor mínimo del rango: ");
+        int min = int.Parse(System.Console.ReadLine());
+
+        System.Console.Write("Ingrese el valor máximo del rango: ");
+        int max = int.Parse(System.Console.ReadLine());
+
+        // Eliminar nodos fuera del rango
+        lista.EliminarFueraDeRango(min, max);
+
+        // Mostrar lista final
+        System.Console.WriteLine("\nLista después de eliminar nodos fuera del rango:");
+        lista.DibujarLista();
+
+        int total = lista.ContarElementos();
+        System.Console.WriteLine("\nCantidad de nodos en la lista: " + total);
+    }
+}
